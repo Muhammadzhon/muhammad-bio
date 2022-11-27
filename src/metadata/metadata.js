cdf3de662c79d78ceb9641af0b51a79f9c126e5b
Muhammadzhon <hmuhammadjon@gmail.com>
2022-11-27 11:45:03 +0600

    content updated

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,45 +1,65 @@
 module.exports = {
-  name: 'Mukhammadzhon Khakimov',
-  title: 'Frontend Developer',
+  name: "Mukhammadzhon Khakimov",
+  title: "Frontend Developer",
   facts: {
-    'Residence': '<a href="https://goo.gl/maps/3pc24sDWgpH8o8tY6"><i class="fa fa-home fact-icon"></i>Osh, Kyrgyzstan</a>',
-    'Linkedin': `<a href="www.linkedin.com/in/mukhammadzhon">
+    Residence:
+      '<a href="https://goo.gl/maps/3pc24sDWgpH8o8tY6"><i class="fa fa-home fact-icon"></i>Osh, Kyrgyzstan</a>',
+    Linkedin: `<a href="www.linkedin.com/in/mukhammadzhon">
                   <i class="fa-brands fa-linkedin fact-icon"></i>Mukhammadzhon 
                 </a>`,
-    'Phone': `<a href="telto:+996555004345"><i class="fa-fa fa-phone fact-icon"></i>+996 (555) 00 43 45</a>`,
-    'Email': '<a href="mailto:hmuhammadjon@gmail.com"><i class="fa fa-envelope fact-icon"></i>hmuhammadjon</a>'
+    GitHub: '<a href="https://github.com/Muhammadzhon"><i class="fab fa-github fact-icon"></i>Muhammadzhon</a>',
+    Phone: `<a href="tel:+996555004345"><i class="fa fa-phone fact-icon"></i>+996 (555) 00 43 45</a>`,
+    Email:
+      '<a href="mailto:hmuhammadjon@gmail.com"><i class="fa fa-envelope fact-icon"></i>hmuhammadjon</a>',
   },
   skills: [
-    ['English', 100],
-    ['Russian', 100],
-    ['Turkish', 100],
-    ['HTML', 100],
-    ['CSS', 100],
-    ['JavaScript', 75],
-    ['React', 75],
-    ['Communication skills', 100],
-    ['Customer service skills', 100],
-    ['Good team work  ', 100],
-    ['Self organized', 100],
+    ["English", 100],
+    ["Russian", 100],
+    ["Turkish", 100],
+    ["HTML", 100],
+    ["CSS", 100],
+    ["JavaScript", 75],
+    ["React", 75],
+    ["Communication skills", 100],
+    ["Customer service skills", 100],
+    ["Good team work  ", 100],
+    ["Self organized", 100],
   ],
   positions: [
     {
-      title: 'Dispatcher',
-      period: '02/2021 – 10/2022',
-      skills: ['Scheduling truck drivers time and route',
-      'Monitoring pick-up, transit and drop off process of the loads',
-      'Solving unexpected issues ',
-      'Quick response for drivers requests',],
-      contents: ``
+      company: "Bilato",
+      location: "Chicago, US",
+      title: "Dispatcher",
+      period: "02/2021 – 10/2022",
+      skills: ['Fluent English', 'Russian', 'Turkish', 'Communication', 'Customer service', 'Team work'],
+      contents: `
+      Bilato holding takes leading position on delivering freight over USA and services 24/7 over 200 trucks and truck drivers.
+
+      As a dispatcher I'm responsible for:
+
+      * Scheduling truck drivers time and route
+      * Monitoring pick-up, transit and drop off process of the loads
+      * Solving unexpected issues 
+      * Quick response for drivers requests
+      `,
     },
     {
-      title: 'Sales Manager',
-      period: '03/2019 – 02/2021',
-      skills: ['Searching cars for purchase',
-      'Diognosing and repairing',
-      'Preparing for Sales',
-      'Dealing with potential buyers face to face and over phone'],
-      contents: ``
-    },    
+      company: 'Mukum Auto',
+      location: 'Osh, Kyrgyz Republic',
+      title: "Sales Manager",
+      period: "03/2017 – 01/2021",
+      skills: ['Analytical skills', 'Customer service', 'Detailing', 'product delivery'],
+      contents: `
+      
+      Mukum Auto has history in auto market over 20 years
+
+      Responsibilities:
+
+      * Searching cars for purchase
+      * Diognosing and repairing
+      * Preparing for Sales
+      * Dealing with potential buyers face to face and over phone      
+      `,
+    },
   ],
 };
