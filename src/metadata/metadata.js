40fd17b9b97d88b04f40ade3a326a794f8848ce3
Muhammadzhon <hmuhammadjon@gmail.com>
2022-11-27 23:43:22 +0600

    skills added

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -7,23 +7,22 @@ module.exports = {
     Linkedin: `<a href="www.linkedin.com/in/mukhammadzhon">
                   <i class="fa-brands fa-linkedin fact-icon"></i>Mukhammadzhon 
                 </a>`,
-    GitHub: '<a href="https://github.com/Muhammadzhon"><i class="fab fa-github fact-icon"></i>Muhammadzhon</a>',
+    GitHub:
+      '<a href="https://github.com/Muhammadzhon"><i class="fab fa-github fact-icon"></i>Muhammadzhon</a>',
     Phone: `<a href="tel:+996555004345"><i class="fa fa-phone fact-icon"></i>+996 (555) 00 43 45</a>`,
     Email:
       '<a href="mailto:hmuhammadjon@gmail.com"><i class="fa fa-envelope fact-icon"></i>hmuhammadjon</a>',
   },
   skills: [
-    ["English", 100],
-    ["Russian", 100],
-    ["Turkish", 100],
-    ["HTML", 100],
-    ["CSS", 100],
-    ["JavaScript", 75],
-    ["React", 75],
-    ["Communication skills", 100],
-    ["Customer service skills", 100],
-    ["Good team work  ", 100],
-    ["Self organized", 100],
+    ["English"],
+    ["Russian"],
+    ["Turkish"],
+    ["HTML"],
+    ["CSS"],
+    ["JavaScript"],
+    ["React"],
+    ["SPA"],
+    ["Github"],
   ],
   positions: [
     {
@@ -31,7 +30,14 @@ module.exports = {
       location: "Chicago, US",
       title: "Dispatcher",
       period: "02/2021 – 10/2022",
-      skills: ['Fluent English', 'Russian', 'Turkish', 'Communication', 'Customer service', 'Team work'],
+      skills: [
+        "Fluent English",
+        "Russian",
+        "Turkish",
+        "Communication",
+        "Customer service",
+        "Team work",
+      ],
       contents: `
       Bilato holding takes leading position on delivering freight over USA and services 24/7 over 200 trucks and truck drivers.
 
@@ -44,11 +50,16 @@ module.exports = {
       `,
     },
     {
-      company: 'Mukum Auto',
-      location: 'Osh, Kyrgyz Republic',
+      company: "Mukum Auto",
+      location: "Osh, Kyrgyz Republic",
       title: "Sales Manager",
       period: "03/2017 – 01/2021",
-      skills: ['Analytical skills', 'Customer service', 'Detailing', 'product delivery'],
+      skills: [
+        "Analytical skills",
+        "Customer service",
+        "Detailing",
+        "product delivery",
+      ],
       contents: `
       
       Mukum Auto has history in auto market over 20 years
